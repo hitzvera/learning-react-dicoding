66beeef9f59479d27b4c7afe361395ab174f5568
Mujahid Ansori Majid <hitzveraidn@gmail.com>
2023-09-12 23:38:49 +0700

    contact app with delete button

--- contact-app/src/components/ContactApp.jsx
+++ contact-app/src/components/ContactApp.jsx
@@ -3,15 +3,44 @@ import React from "react";
 import ContactList from "./ContactList";
 import { getData } from "../utils/data";
 
-function ContactApp() {
-  const contacts = getData();
 
-  return (
-    <div className="contact-app">
-      <h1>Daftar Kontak</h1>
-      <ContactList contacts={contacts} />
-    </div>
-  );
+
+class ContactApp extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      contacts: getData(),
+    };
+
+    this.onDeleteHandler = this.onDeleteHandler.bind(this);
+  }
+  onDeleteHandler(id) {
+    const contacts = getData().filter((data) => data.id !== id);
+    this.setState({ contacts });
+  }
+  render() {
+    return (
+      <div className="contact-app">
+        <h1>Daftar Kontak</h1>
+        <ContactList
+          contacts={this.state.contacts}
+          onDelete={this.onDeleteHandler}
+        />
+      </div>
+    );
+  }
 }
+
+// function ContactApp() {
+//   const contacts = getData();
+
+//   return (
+//     <div className="contact-app">
+//       <h1>Daftar Kontak</h1>
+//       <ContactList contacts={contacts} />
+//     </div>
+//   );
+// }
 
 export default ContactApp;
---
+++ contact-app/src/components/DeleteButton.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+
+function DeleteButton({ id, onDelete }) {
+    return <button className='contact-item__delete' onClick={()=>onDelete(id)}>X</button>
+}
+
+export default DeleteButton
