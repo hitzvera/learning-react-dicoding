1b718d9352df0778a17c5d615777cffd1629e3bc
Mujahid Ansori Majid <hitzveraidn@gmail.com>
2023-09-12 22:42:08 +0700

    finishing contact app

---
+++ contact-app/src/components/ContactItem.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React from "react";
+
+import ContactItemBody from "./ContactItemBody";
+import ContactItemImage from "./ContactItemImage";
+
+function ContactItem({ imageUrl, name, tag }) {
+  return (
+    <div className="contact-item">
+      <ContactItemImage imageUrl={imageUrl} />
+      <ContactItemBody name={name} tag={tag} />
+    </div>
+  );
+}
+
+export default ContactItem;
